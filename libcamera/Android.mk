bc6fc3804bf2f37ec35f2a28f64e32774e9711f1
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-01-05 18:43:50 +0400

    Camera HAL optimizations
    Change-Id: I17e3d18d5c2dfaa6bdea2dadfe073e19d87c7c36

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -2,19 +2,31 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS           += -O3
-LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
 
-LOCAL_MODULE_TAGS      := optional
+LOCAL_SRC_FILES := QualcommCameraHardware.cpp
+LOCAL_SRC_FILES += cameraHAL.cpp
 
-LOCAL_SRC_FILES        := cameraHAL.cpp
-LOCAL_C_INCLUDES       := $(TOP)/frameworks/base/include
-LOCAL_C_INCLUDES       += hardware/qcom/display-legacy/libgralloc
+LOCAL_CFLAGS  := -DDLOPEN_LIBMMCAMERA=1
+LOCAL_CFLAGS += -DNUM_PREVIEW_BUFFERS=4
 
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
-LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+ifeq ($(BOARD_DEBUG_MEMLEAKS),true)
+    LOCAL_CFLAGS += -DHEAPTRACKER
+endif
 
-LOCAL_LDFLAGS	       += -L$(LOCAL_PATH) -lcamera
+LOCAL_C_INCLUDES := $(TOP)/frameworks/base/include
+LOCAL_C_INCLUDES += hardware/qcom/display-legacy/libgralloc
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-camera
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-still/jpeg
+
+LOCAL_SHARED_LIBRARIES := libutils libui libcamera_client liblog libcutils
+LOCAL_SHARED_LIBRARIES += libbinder libdl libhardware
+
+ifeq ($(BOARD_DEBUG_MEMLEAKS),true)
+    LOCAL_SHARED_LIBRARIES += libheaptracker
+endif
 
 include $(BUILD_SHARED_LIBRARY)

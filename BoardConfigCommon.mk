01065014e285e51e5199fd0a4b0461568a6b54dd
Conn O'Griofa <connogriofa@gmail.com>
2013-04-12 20:54:19 -0300

    ath6kl: clean target before building
    Should avoid problems if a prior build failed before cleaning up.
    
    Change-Id: I56cecbc48c2e3eed86d8de4ec5f6304d7fe8934a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,15 +107,22 @@ ifeq ($(BOARD_WLAN_DEVICE),ath6kl_compat)
 
 	# Build the ath6kl-compat modules
 KERNEL_EXTERNAL_MODULES:
+	# clean up ath6kl-compat target
+	make -C hardware/atheros/ath6kl-compat KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" clean
+	rm hardware/atheros/ath6kl-compat/include/linux/compat_autoconf.h
+	# run build
 	make -C hardware/atheros/ath6kl-compat KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-"
+	# strip modules to economize space
 	$(ANDROID_TOOLCHAIN)/arm-linux-androideabi-objcopy --strip-unneeded hardware/atheros/ath6kl-compat/compat/compat.ko
 	$(ANDROID_TOOLCHAIN)/arm-linux-androideabi-objcopy --strip-unneeded hardware/atheros/ath6kl-compat/drivers/net/wireless/ath/ath6kl/ath6kl.ko
 	$(ANDROID_TOOLCHAIN)/arm-linux-androideabi-objcopy --strip-unneeded hardware/atheros/ath6kl-compat/net/wireless/cfg80211.ko
+	# move modules to system image
 	mv hardware/atheros/ath6kl-compat/compat/compat.ko $(KERNEL_MODULES_OUT)
 	mv hardware/atheros/ath6kl-compat/drivers/net/wireless/ath/ath6kl/ath6kl.ko $(KERNEL_MODULES_OUT)
 	mv hardware/atheros/ath6kl-compat/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
-	rm hardware/atheros/ath6kl-compat/include/linux/compat_autoconf.h
+	# clean up ath6kl-compat target
 	make -C hardware/atheros/ath6kl-compat KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" clean
+	rm hardware/atheros/ath6kl-compat/include/linux/compat_autoconf.h
 TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
 else
 	# Enhance Samsung AR6000 compatibility

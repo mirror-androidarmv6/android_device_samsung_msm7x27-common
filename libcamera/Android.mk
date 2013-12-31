bf6bdd7ea48c84f5e9b7205097767321eb2db6f4
Conn O'Griofa <connogriofa@gmail.com>
2013-12-31 20:40:15 +0100

    msm7x27-common: Update Camera HAL to new version
    libcamera: update camera HAL to newer version
    
    Includes workaround to prevent NPEs related to missing saturation/
    sharpness/contrast parameters, and supports 5MP cameras.
    
    Adapted from: https://github.com/Evervolv/android_hardware_qcom_camera/tree/jellybean-4.1.2
    Commit: ea5ddb597033320dcd589fa0bc443fa11626b98d
    
    Change-Id: I02a4efa63548162ea37762eb240eb56428910437

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,26 +1,39 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),)
+    DISPLAY := display-$(TARGET_QCOM_DISPLAY_VARIANT)
+else
+    DISPLAY := display
+endif
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS           += -O3
+LOCAL_C_FLAGS          += -O3
+LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_MODULE_TAGS      := optional
+LOCAL_SRC_FILES        := QcomCamera.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui
+
+LOCAL_LDFLAGS          += -Lvendor/samsung/msm7x27-common/proprietary/lib -lcamera
+
+LOCAL_C_INCLUDES       := frameworks/base/services \
+                          frameworks/base/include \
+                          hardware/libhardware/include
+
+LOCAL_C_INCLUDES       += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_CFLAGS           := -DPREVIEW_MSM7K
+LOCAL_C_INCLUDES       += hardware/qcom/$(DISPLAY)/libgralloc
+
+LOCAL_PRELINK_MODULE   := false
 
 ifneq (,$(filter beni cooper,$(CM_BUILD)))
     LOCAL_CFLAGS       += -DBOARD_CAMERA_5MP
 endif
-
-LOCAL_SRC_FILES        := cameraHAL.cpp
-LOCAL_C_INCLUDES       := $(TOP)/frameworks/base/include
-LOCAL_C_INCLUDES       += hardware/qcom/display-legacy/libgralloc
-
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
-LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
-LOCAL_C_INCLUDES       += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-LOCAL_LDFLAGS          += -Lvendor/samsung/msm7x27-common/proprietary/lib -lcamera
 
 include $(BUILD_SHARED_LIBRARY)
